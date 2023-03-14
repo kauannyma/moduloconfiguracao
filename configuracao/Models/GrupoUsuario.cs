c7ea32a4ffee5d16fdd12910652901eb9989edde
kauanny maria <m.kauannyaraujo@gmail.com>
2023-03-14 16:29:36 -0300

    Alterações

---
+++ configuracao/Models/GrupoUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class GrupoUsuario
+    {
+        public int Id { get; set; }
+        public string NomeGrupo { get; set; }
+
+        List<Permissao> Permissoes { get; set; }
+    }
+}
---
+++ configuracao/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public  string NomeUsuario { get; set; } 
+        
+        public int Senha { get; set; }
+
+        public string Email { get; set; }   
+
+        public string  CPF { get; set; }
+        public bool Ativo { get; set; }
+
+       public  List<GrupoUsuario> GrupoUsuarios { get; set; }
+
+
+
+
+    }
+}
